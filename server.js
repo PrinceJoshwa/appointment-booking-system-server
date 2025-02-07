9573ad610a4814258c47ae9fa230d981fd78bc15
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-02-07 12:24:04 +0530

    server.js changes

--- server.js
+++ server.js
@@ -10,10 +10,20 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
+// app.use(cors({
+//     origin: ['https://appointment-booking-system-iota.vercel.app', 'http://localhost:5173'],
+//     credentials: true
+// }));
+
 app.use(cors({
-    origin: ['https://appointment-booking-system-iota.vercel.app', 'http://localhost:5173'],
+    origin: [
+      process.env.CLIENT_URL,
+      'http://localhost:5173'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
-}));
+  }));
 
 app.use("/api/appointments", appointmentRoutes)
 
