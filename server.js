d160ce52642dc48e853f5a580ec93d946ea6464e
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-02-07 12:07:55 +0530

    deploy commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://appointment-booking-system-iota.vercel.app', 'http://localhost:5173'],
     credentials: true
 }));
 
